dabdf8e1799aae864d6af3b01ad81db40d03aa14
Jahangir Shabiev <c.shabiev@gmail.com>
2017-05-24 18:20:39 +0400

    DB settings

--- smart_proj/settings.py
+++ smart_proj/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'smart_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'db',
+        'NAME': 'accountdb',
+        'USER': 'accountuser',
+        'PASSWORD': 'accountuser'
     }
 }
 
